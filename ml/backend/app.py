e429e08ebcb7656ae9d7ab16b17d2f597341755c
Slava Sokolov <sslavvassokolovv@gmail.com>
2023-09-14 16:15:41 +0300

    1

--- ml/backend/app.py
+++ ml/backend/app.py
@@ -1,5 +1,8 @@
 from enum import Enum
 
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+import psycopg2
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
 
@@ -17,17 +20,51 @@ class IrisModel(BaseModel):
     sepal_height: float = Field(ge=0)
     sepal_width: float = Field(ge=0)
 
+def fetch_data() -> pd.DataFrame:
+    query = """
+    SELECT iris_id, petal_height, petal_width, sepal_height, sepal_width, target
+    FROM Iris
+    """
+
+    with psycopg2.connect(
+        dbname="iris",
+        user='admin',
+        password='admin',
+        host='db'
+    ) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            rows = cursor.fetchall()
+            return pd.DataFrame(
+                rows,
+                columns=[
+                    "iris_id",
+                    "petal_height",
+                    "petal_width",
+                    "sepal_height",
+                    "sepal_width",
+                    "target",
+                ],
+            ).set_index("iris_id")
+
+def get_predict(data: pd.DataFrame, iris: IrisModel) -> int:
+    model = KNeighborsClassifier()
+    model.fit(X=data[["petal_height", "petal_width", "sepal_height", "sepal_width"]], y = data["target"])
+    return model.predict(pd.DataFrame(data=iris.model_dump(), index=[0]).values)[0]
+
+
 @app.get("/healthcheck")
 def healthcheck():
     return {"status": "OK"}
 
 @app.get("/predict")
 def predict(iris: IrisModel):
-    score = sum([iris.petal_height, iris.petal_width, iris.sepal_height, iris.sepal_width])
-    if score > 10:
-        iris_type = "foo"
-    elif score > 5:
-        iris_type = IrisType.VERSICOLOUR
-    else:
-        iris_type = IrisType.VIRGINICA
-    return {"predict": iris_type}
+    data = fetch_data()
+    predict = get_predict(data=data, iris=iris)
+
+    iris_type = {
+        0: IrisType.SETOSA,
+        1: IrisType.VERSICOLOUR,
+        2: IrisType.VIRGINICA,
+    }[predict]
+    return  {"predict": iris_type}
